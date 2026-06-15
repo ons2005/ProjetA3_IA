a01e7ce88629fcd3df4bc9edac7d1907b516ee6d
Etes-Hyene <etienne.decamps@isen-ouest.yncrea.fr>
2026-06-15 19:38:19 +0200

    grid search

--- Besoin_Client_4/prediction_puissance.py
+++ Besoin_Client_4/prediction_puissance.py
@@ -1,13 +1,14 @@
 import pandas as pd
+from sklearn.metrics import mean_squared_error, root_mean_squared_error
 
-
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.preprocessing import  OrdinalEncoder
 from sklearn.tree import DecisionTreeRegressor
 
 import matplotlib.pyplot as plt
 
-file_path = "BDD_nettoyé_IA.csv"
+
+file_path = "Besoin_Client_4/BDD_nettoyé_IA.csv"
 
 data=pd.read_csv(file_path,encoding="utf8",sep=",")
 
@@ -25,14 +26,46 @@ dataX= data[["implantation_station","prise_type_ef","prise_type_2","prise_type_c
 dataY= data["puissance_nominale"].copy()
 
 
-# divise la base données en base d'apprentissage et de test
-train_setX, test_setX = train_test_split(dataX,test_size=0.2,random_state=42)
+# divise la base données en base d'apprentissage, de validation et de test
 
-train_setY, test_setY = train_test_split(dataY,test_size=0.2,random_state=42)
+Xtrain, Xvalidtest = train_test_split(dataX,train_size=0.6)
+Xvalid, Xtest = train_test_split(Xvalidtest,train_size=0.5)
 
+ytrain, yvalidtest = train_test_split(dataY,train_size=0.6)
+yvalid, ytest = train_test_split(yvalidtest,train_size=0.5)
 
 tree= DecisionTreeRegressor()
 
-tree.fit(X=dataX,y=dataY)
+tree.fit(X=Xtrain,y=ytrain)
+
+ypred= tree.predict(X=Xtest)
+
+print(pow(mean_squared_error(y_true=ytest, y_pred=ypred),exp=0.5))
+
+
+param_grid = {
+    'max_features': ['auto', 'sqrt', 'log2'],
+    'max_depth': [None,4, 5, 6, 7, 8],
+    'criterion': ['squared_error', 'absolute_error', 'poisson'],
+    'splitter': ['best','random']
+}
+
+grid= GridSearchCV(estimator=tree,param_grid=param_grid,scoring="neg_root_mean_squared_error")
+
+# Fit the model
+grid.fit(Xtrain, ytrain)
+
+# Get the best parameters and score
+print("Best parameters found: ", grid.best_params_)
+print("Best cross-validation score: ", grid.best_score_)
+
+# Predict on the test set
+y_pred = grid.predict(Xtest)
+
+# Evaluate the model
+score = root_mean_squared_error(ytest, ypred)
+print("score RMSE: ", score)
+
+
 
 
