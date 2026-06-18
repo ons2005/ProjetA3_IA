29c03c7f12589cfc3072614c20423e9cfa34dcc8
Etes-Hyene <etienne.decamps@isen-ouest.yncrea.fr>
2026-06-18 16:36:58 +0200

    fin besoin 4

--- Besoin_Client_4/prediction_puissance.py
+++ Besoin_Client_4/prediction_puissance.py
@@ -1,43 +1,49 @@
 
 import joblib as jb
-import numpy as np
 import pandas as pd
-from sklearn.metrics import root_mean_squared_error
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 
-file_path = "BDD_nettoyé_IA.csv"
+#importation du modèle
 
-data=pd.read_csv(file_path,encoding="utf8",sep=",",low_memory=False)
-
-data = data.dropna()
-encoder = OrdinalEncoder()
-
-dataY = data["puissance_nominale"].copy()
-dataX= data.drop(columns="puissance_nominale")
-
-for index in dataX.keys():
-    dataX[[index]]= encoder.fit_transform(dataX[[index]])
-
-dataX= pd.DataFrame(dataX)
-
-Xtrain, Xtest, ytrain, ytest = train_test_split(dataX, dataY, test_size=0.2, random_state=42)
 model= jb.load("modèle_puissance.pkl")
 
-def predict(dataa):
+#création de données d'implantation
 
-    values = dataa[
-        ['datagouv_dataset_id', 'implantation_station', 'nom_operateur', 'prise_type_combo_ccs', 'nom_amenageur',
-         'datagouv_organization_or_owner', 'prise_type_chademo', 'prise_type_2', 'restriction_gabarit',
-         'contact_amenageur']].copy()
+data={
+    "datagouv_dataset_id": "6740a4d4ca0ce2aba15a3c68",
+    "implantation_station": "Voirie",
+    "nom_operateur": "SOLVEO ENERGIES",
+    "prise_type_combo_ccs": True,
+    "nom_amenageur": "SOLVEO ENERGIES",
+    "datagouv_organization_or_owner": "solveo-energies",
+    "prise_type_chademo": True,
+    "prise_type_2": False,
+    "restriction_gabarit": "max 2m",
+    "contact_amenageur": "t.prouin@sde23.fr"
+}
+
+data= pd.DataFrame(data, index=[0])
+
+#fonction de prédiction de la puissance nominale
+
+def predict(dataa):
+    encoder = OrdinalEncoder()
+
+    # conversion des données aux format numérique
+    for i in dataa.keys():
+        dataa[[i]] = encoder.fit_transform(dataa[[i]])
+
+    # prédiction de la puissance
+    values = dataa[["datagouv_dataset_id", "implantation_station", "nom_operateur", "prise_type_combo_ccs", "nom_amenageur",
+         "datagouv_organization_or_owner", "prise_type_chademo", "prise_type_2", "restriction_gabarit",
+         "contact_amenageur"]].copy()
 
     return model.predict(values)
 
 if __name__ == "__main__":
 
-  ypred= predict(Xtest)
+    ypred= predict(data)
 
-for index in range(len(ypred)-1):
-        print(f"puissance réelle {(ytest.values[index])} , puissance prédite: {ypred[index]}")
+    for index in range(len(ypred)):
+            print(f"puissance prédite: {ypred[index]}")
 
-print(f"score RMSE:{root_mean_squared_error(y_pred=ypred,y_true=ytest)}")
